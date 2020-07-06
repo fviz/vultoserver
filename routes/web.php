c685a540ade859d18add1b467fb9690aa8f786b6
shady <franciscovizentin@gmail.com>
2020-07-06 19:46:07 +0200

    added buffer logging

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/save_buffer', function(Request $request) {
+    \Illuminate\Support\Facades\Log::info("Received buffer.");
+    return $request->buffer;
 });
