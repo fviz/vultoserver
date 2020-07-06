b80afb2f4e5c5165e48ea38f50dad14614ba1444
shady <franciscovizentin@gmail.com>
2020-07-06 20:23:33 +0200

    Updated route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('/save_buffer', function(Request $request) {
-    \Illuminate\Support\Facades\Log::info("Received buffer.");
-    return $request->buffer;
-});
